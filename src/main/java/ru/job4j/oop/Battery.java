de5a7f2136838915d3156e821c7f751767e12fb6
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-07 00:02:28 +0300

    Состояние объекта [#167388 #340380]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,12 +3,12 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
-    public Battery(int load) {
-        this.load = load;
-    }
-
     public int getLoad() {
         return load;
+    }
+
+    public Battery(int load) {
+        this.load = load;
     }
 
     public void exchange(Battery another) {
