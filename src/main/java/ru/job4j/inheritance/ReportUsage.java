ba322598fed86f774826492faa3371bdbeb6fa8e
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-08 19:43:08 +0300

    Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -4,11 +4,15 @@ public class ReportUsage {
     public static void main(String[] args) {
         TextReport reportText = new TextReport();
         HtmlReport reportHtml = new HtmlReport();
+        JSONReport reportJson = new JSONReport();
         String text = reportText.generate("Report's name", "Report's body");
         System.out.println(text);
         System.out.println();
         String html = reportHtml.generate("Report's name", "Report's body");
         System.out.println(html);
+        System.out.println();
+        String json = reportJson.generate("Report's name", "Report's body");
+        System.out.println(json);
 
     }
 }
