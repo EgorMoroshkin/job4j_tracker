998f6919b48af962951d5fea6f412be24d2c6750
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-08 19:34:56 +0300

    Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheese extends Pizza {
+    private String name = "extra cheese";
+
+    @Override
+    public String name() {
+        return super.name() + " " + name;
+
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
+    private String name = "extra tomato";
+
+    @Override
+    public String name() {
+        return super.name() + " " + name;
+
+    }
+}
