af2e7f8056c53c032a841409906df29399353f8a
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-11 14:26:02 +0300

    toString [#310018]

--- src/main/java/ru/job4j/inheritance/StartUI.java
+++ src/main/java/ru/job4j/inheritance/StartUI.java
@@ -1,14 +1,10 @@
 package ru.job4j.inheritance;
 
-
 import ru.job4j.tracker.Item;
-
-import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getTime().format(formatter));
+        System.out.println(item);
     }
 }
