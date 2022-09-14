cf6b4ff351a2607e33f5b8c21979f52ac1a57867
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-14 19:56:16 +0300

    Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
+        String answerQuestion = scr.nextLine();
+        String answerClient = "";
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0:
+                answerClient = "Да";
+                break;
+            case 1:
+                answerClient = "Нет";
+                break;
+            case 2:
+                answerClient = "Может быть";
+                break;
+            default:
+        }
+        System.out.println(answerClient);
+    }
+}
