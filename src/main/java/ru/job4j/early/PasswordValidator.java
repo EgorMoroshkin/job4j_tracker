b3dc7926695bfc6082a888b002fb96e9c32d8e92
Морошкин Егор Владимирович <EVMoroshkin@sberbank.ru>
2022-09-26 15:40:58 +0300

    Принципы раннего возврата и охранных выражений [#504875 #346465]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class PasswordValidator {
     public static String validate(String password) {
         String rls = "Success";
-        if (password.isEmpty()) {
+        String[] array = new String[]{"qwerty", "12345", "password", "admin", "user"};
+        if (password == null || password.isEmpty()) {
             throw new IllegalArgumentException("Пароль не задан");
         }
         if (password.length() < 8 || password.length() > 32) {
@@ -17,33 +18,31 @@ public class PasswordValidator {
         if (password.equals(password.toLowerCase())) {
             throw new IllegalArgumentException("Пароль должен содержать хотя бы один символ в верхнем регистре");
         }
-        if (password.toLowerCase().contains("qwerty") || password.toLowerCase().contains("12345") || password.toLowerCase().contains("password")
-                || password.toLowerCase().contains("admin") || password.toLowerCase().contains("user")) {
-            throw new IllegalArgumentException("Пароль содержит подстроки без учета регистра: qwerty, 12345, password, admin, user.");
+        for (int i = 0; i < array.length; i++) {
+            if (password.toLowerCase().contains(array[i])) {
+                throw new IllegalArgumentException("Пароль содержит подстроки без учета регистра: qwerty, 12345, password, admin, user.");
+            }
         }
         char[] arrayChar = password.toCharArray();
-        while (true) {
-            boolean num = true;
-            boolean symbol = true;
-            for (int i = 0; i < arrayChar.length; i++) {
-                if (Character.isDigit(arrayChar[i])) {
-                    num = false;
-                    break;
-                }
+        boolean num = true;
+        boolean symbol = true;
+        for (int i = 0; i < arrayChar.length; i++) {
+            if (Character.isDigit(arrayChar[i])) {
+                num = false;
+                break;
             }
-            if (num) {
-                throw new IllegalArgumentException("Пароль должен содержать хотя бы одну цифру");
+        }
+        if (num) {
+            throw new IllegalArgumentException("Пароль должен содержать хотя бы одну цифру");
+        }
+        for (int i = 0; i < arrayChar.length; i++) {
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                symbol = false;
+                break;
             }
-            for (int i = 0; i < arrayChar.length; i++) {
-                if (!Character.isLetterOrDigit(password.charAt(i))) {
-                    symbol = false;
-                    break;
-                }
-            }
-            if (symbol) {
-                throw new IllegalArgumentException("Пароль должен содержать хотя бы один спец. символ (не цифра и не буква)");
-            }
-            break;
+        }
+        if (symbol) {
+            throw new IllegalArgumentException("Пароль должен содержать хотя бы один спец. символ (не цифра и не буква)");
         }
         return rls;
     }
