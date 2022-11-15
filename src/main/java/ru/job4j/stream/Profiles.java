7b9ad3101edb339cf0b4228a95e4ce33210b5b21
EgorMoroshkin <Westwest38!>
2022-11-15 19:36:32 +0300

    Список адресов [#110062 #360839]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,7 +8,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(x -> new Profile(x.getAddress()).getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
